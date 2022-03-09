c325084d2520a1dc5f7a7ef832b10fabba274bc5
Jerrin Eldo Mazhuvancherry P <jerin.jollybhavan@gmail.com>
2022-03-08 20:59:10 -0500

    first commit

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,55 @@ import { OrderCalculationResult } from './models/OrderCalculationResult';
 import { Submission } from './models/Submission';
 import { SubmissionResponse } from './models/SubmissionResponse';
 
-const YourEmail = 'youremail@gmail.com'
+const YourEmail = 'jerin.jollybhavan@gmail.com'
 
 async function getOrderLines(): Promise<OrderLine[]> {
-  const response = await Axios.get<OrderLine[]>("https://archoninterviewssoftwaredeveloper.azurewebsites.net/api/order-lines/problem1/sample")
+  const response = await Axios.get<OrderLine[]>("https://archoninterviewssoftwaredeveloper.azurewebsites.net/api/order-lines/problem2/sample")
   return response.data
 }
 
-function calculate(orderLines: OrderLine[]): OrderCalculationResult[] {
-  
-  try{
-  var arr : OrderCalculationResult[] = []
-  var length = 0
+function calculate (orderLines: OrderLine[]): OrderCalculationResult[] {
+  try {
+    var arr: OrderCalculationResult[] = []
+    var length = 0
 
-  for (const order of orderLines) {
-    arr[length] = { 
-      orderId : order.orderId,
-      total : order.quantity * order.unitPrice
-    } 
-    length++
+    //console.log('orderLines :>> ', orderLines)
+
+    for (const order of orderLines) {
+      arr[length] = {
+        orderId: order.orderId,
+        total: order.quantity * order.unitPrice,
+        buy2Get1FreeTotal : (order.quantity - Math.floor(order.quantity/3)) * order.unitPrice
+      }
+      length++
+    }
+
+    console.log('arr>>', arr)
+
+    var holder = {}
+    arr.forEach(function (d) {
+      if (holder.hasOwnProperty(d.orderId)) {
+        holder[d.orderId][0] = holder[d.orderId][0] + d.total
+        holder[d.orderId][1] = holder[d.orderId][1] + d.buy2Get1FreeTotal
+      } else {
+        holder[d.orderId] = []
+        holder[d.orderId].push(d.total)
+        holder[d.orderId].push(d.buy2Get1FreeTotal)
+      }
+    })
+    //console.log('holder>>', holder)
+
+    var obj2 = []
+
+    for (var prop in holder) {
+      obj2.push({ orderId: Number(prop), total: holder[prop][0] , buy2Get1FreeTotal: holder[prop][1]  })
+    }
+    console.log('arr', obj2)
+    return obj2
+  } catch (err) {
+    console.error('Something is wrong', { err })
+    console.error(err)
   }
-
-  return arr
-}catch (err) {
-  console.error("Something went wrong")
-  console.error(err)
-}
-  
 }
 
 async function submitResults(results: OrderCalculationResult[]) {
@@ -39,7 +61,7 @@ async function submitResults(results: OrderCalculationResult[]) {
     results
   }
 
-  const response = await Axios.post<SubmissionResponse>("https://archoninterviewssoftwaredeveloper.azurewebsites.net/api/results/problem1/sample", submission)
+  const response = await Axios.post<SubmissionResponse>("https://archoninterviewssoftwaredeveloper.azurewebsites.net/api/results/problem2/sample", submission)
   console.log(response.data.allBuyTwoGetOneFreeTotalsAreCorrect ? "Buy 2 get 1 free totals are correct" : "Buy 2 get 1 free totals are not correct")
   console.log(response.data.allOrderTotalsAreCorrect ? "Totals are correct" : "Totals are not correct")
   for (const hint of response.data.hints) {
--- src/models/OrderCalculationResult.ts
+++ src/models/OrderCalculationResult.ts
@@ -1,4 +1,5 @@
 export interface OrderCalculationResult {
   orderId: string
   total: number
+  buy2Get1FreeTotal : number
 }
